f59276a8021e08a069efee6fb4f83ce60221df48
firanblla <firanblla13@gmail.com>
2023-10-30 16:45:42 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(title: 'Flutter Demo',
+      theme: ThemeData( primarySwatch: Colors.blue,
+      ),
+        home: MyHomePage(title: 'Product layout demo home page'),
+    );}}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({ Key? key, required this.title}):super(key: key);
+  final String title;
+  @override Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product Listing")),
+      body: ListView( shrinkWrap: true, padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+        children: <Widget>[
+          ProductBox(name: "Iphone 15 Pro", description: "Iphone is the stylist phone ever",
+            price: 1000, image: "iphone 15.jpg"),
+          ProductBox(name: "Pixel 6 Pro", description: "Pixel is the most featureful phone ever",
+            price: 800, image: "pixel 6 pro.jpg"),
+          ProductBox(name: "Laptop Lenovo Ideapad 330", description: "Laptop is the most productive development tool",
+            price: 2000, image: "laptop.jpg"),
+          ProductBox(name: "Tablet Samsung", description: "Tablet is the most useful device ever for meeting",
+            price: 1500, image: "tablet.jpg"),
+          ProductBox(name: "Mouse Logitech", description: "The mouse is very agile",
+            price: 100, image: "mouse.jpg"),
+          ProductBox( name: "Keyboard", description: "The keyboard is nice to press",
+              price: 20, image: "keyboard.jpg"),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductBox extends StatelessWidget {
+  const ProductBox({Key? key, required this.name, required this.description,
+  required this.price, required this.image}):super(key:key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  
+  Widget build(BuildContext context) {
+    return Container( padding: EdgeInsets.all(2), height: 140,
+      child: Card( child: Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[ Image.asset("image/"+image),
+          Expanded( child: Container(padding: EdgeInsets.all(5),
+            child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[ Text(this.name, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(this.description), Text("Price: " + this.price.toString()),],))) ])));
+  }
+}
